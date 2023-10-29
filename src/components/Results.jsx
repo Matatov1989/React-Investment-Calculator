2fd0abe76f550fa632f24d90b8fb3c167e048362
Yury <matatov1989@gmail.com>
2023-10-29 16:06:30 +0200

    computing valuers & properly handing number values

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,9 @@
+import { calculateInvestmentResults } from "../util/investment";
+
+
 export default function Results({ input }) {
+    
+    const resultsData = calculateInvestmentResults(input);
     return (
         <p>Results...</p>
     );
